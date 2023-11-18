3a58c4599578e18fdd17799a6d2e7bb5cabcff5b
ovto <ovtyne@gmail.com>
2023-11-18 17:14:31 +0400

    course 8

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER')
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'POSTGRES_HOST_AUTH_METHOD': 'trust'
     }
 }
 
